7893ffb85bcc20461fcc322b6403ff1e87eb9662
Abishan1127 <kunananthan19abishan@gmail.com>
2025-05-09 11:28:31 +0530

    server updated

--- server.js
+++ server.js
@@ -33,14 +33,6 @@ app.use('/api/auth', authRoutes);
 app.use('/api/shifts', authenticateJWT, shiftRoutes);
 app.use('/api/admin', authenticateJWT, adminRoutes);
 
-// Serve static files in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-  });
-}
 
 // Error handling middleware
 app.use(handleError);
